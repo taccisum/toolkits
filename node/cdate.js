b91cd54a1a14a2a93828fd1d8c64c41e6aaf97aa
taccisum <liaojinfeng@deepexi.com>
2022-08-25 14:48:44 +0800

    +: new cli tool 'cdate'

---
+++ node/cdate.js
@@ -0,0 +1,39 @@
+#!/usr/local/bin/node
+
+const fs = require('fs')
+const path = require('path')
+
+const { Command, Argument }  = require('commander')
+
+let program = new Command();
+
+let pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
+program.version(pkg.version, '-v --version');
+program.description(pkg.description);
+program.usage('[command] [options]');
+
+function print_date(d) {
+    console.log('Date Time: ', d)
+    console.log('Timestamp(s): ', Math.floor(d.getTime() / 1000))
+    console.log('Timestamp(ms): ', d.getTime())
+}
+
+program.command('convert', {isDefault: true})
+.description('')
+.argument('[date]', 'date string or timestamp')
+.action(async (date, opts) => {
+    let d
+    if (!date) {
+        d = new Date()
+    } else {
+        const tstamp = Number.parseInt(date)
+        if (tstamp) {
+            d = new Date(tstamp)
+        } else {
+            d = new Date(date)
+        }
+    }
+    print_date(d)
+});
+
+program.parse()

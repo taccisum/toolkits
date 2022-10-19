433a2fe04a2363737d88fe80a3258f7fbcfae096
taccisum <liaojinfeng6937@dingtalk.com>
2022-10-19 16:35:15 +0800

    feat: support detect dependency pom.xml in child module for upgrade

--- node/mvn_patch/upgrade.js
+++ node/mvn_patch/upgrade.js
@@ -1,57 +1,67 @@
 const fs = require('fs')
+const path = require('path')
 const execa = require('execa')
 const chalk = require('chalk')
 
+function _upgrade(pom, groupId, artifactId, newVersion) {
+    const lines = String(fs.readFileSync(pom)).split('\n')
+
+    let state = 0
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        switch (state) {
+            case 0:
+                if (line.match(`.*<groupId>${groupId}</groupId>`)) {
+                    state = 1
+                }
+                break
+            case 1:
+                if (line.match(`.*<artifactId>${artifactId}</artifactId>`)) {
+                    state = 2
+                } else {
+                    state = 0
+                }
+                break
+            case 2:
+                console.log(chalk.bold.green('Found dependency. Do upgrade now...'))
+                if (line.match(/.*<version>.*<\/version>/)) {
+                    lines[i] = line.replace(/<version>.*<\/version>/, `<version>${newVersion}</version>`)
+                    state = 3
+                }
+                break
+            default:
+                break
+        }
+
+        if (state === 3) {
+            break
+        }
+    }
+
+    fs.writeFileSync(pom, lines.reduce((p, n) => `${p}\n${n}`))
+}
 
 module.exports = {
     async handlePatch(patch, opts, tmpDir) {
+        const { groupId, artifactId } = patch
         const { version: newVersion } = patch.upgrade
-        const { groupId, artifactId, version } = patch
 
-        const pomPath = 'open-platform-gateway-provider/pom.xml'
-
-        const lines = String(fs.readFileSync(pomPath)).split('\n')
-
-        let state = 0
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-
-            switch (state) {
-                case 0:
-                    if (line.match(`.*<groupId>${groupId}</groupId>`)) {
-                        state = 1
-                    }
-                    break
-                case 1:
-                    if (line.match(`.*<artifactId>${artifactId}</artifactId>`)) {
-                        state = 2
-                    } else {
-                        state = 0
-                    }
-                    break
-                case 2:
-                    console.log(chalk.bold.green('Found dependency. Do upgrade now...'))
-                    if (line.match(/.*<version>.*<\/version>/)) {
-                        lines[i] = line.replace(/<version>.*<\/version>/, `<version>${newVersion}</version>`)
-                        state = 3
-                    }
-                    break
-                default:
-                    break
-            }
-
-            if (state === 3) {
-                break
-            }
+        const poms = fs.readdirSync(tmpDir)
+            .filter(f => !f.startsWith('.'))        // ignore hidden dir & parent dir
+            .filter(f => fs.statSync(f).isDirectory())      // ignore non-directory file
+            .map(d => path.join(d, 'pom.xml'))      // map to relative pom path
+            .filter(pom => fs.existsSync(pom))      // ignore pom not existed 
+        if (fs.statSync('pom.xml').isFile()) {
+            poms.unshift('pom.xml')     // add root pom.xml
         }
 
-        if (state !== 3) {
-            console.log(chalk.bold.bgRed('Dependency not found.'))
-            process.exit()
-        }
+        poms.forEach(pom => {
+            // for each all pom.xml n' find target dependency to upgrade
+            _upgrade(pom, groupId, artifactId, newVersion)
+        })
 
-        fs.writeFileSync(pomPath, lines.reduce((p, n) => `${p}\n${n}`))
-
+        // show changes
         await execa('git', [ 'diff'], {
             stdin: process.stdin, stdout: process.stdout, stderr: process.stderr
         })

f1487c1dba7de8a3fa34c50caeeb169be2bc1ba3
taccisum <liaojinfeng6937@dingtalk.com>
2023-08-10 15:21:59 +0800

    feat: support parse utc date string for cdate command

--- node/cdate.js
+++ node/cdate.js
@@ -13,28 +13,43 @@ program.description(pkg.description);
 program.usage('[command] [options]');
 
 function print_date(d) {
-    console.log('Date Time: ', d.toLocaleString('zh-cn'))
+    console.log('Date Time(utc): ', d.toISOString())
+    console.log('Date Time(zh-cn): ', d.toLocaleString('zh-cn'))
     console.log('Timestamp(s): ', Math.floor(d.getTime() / 1000))
     console.log('Timestamp(ms): ', d.getTime())
+}
+
+function parse_date(date) {
+    process.env.TZ = 'Asia/Shanghai'
+    let d
+    if (!date) {
+        d = new Date()
+    } else {
+        if (String(date).startsWith('u')) {
+            d = new Date(date.slice(1,))        // utc string
+        } else if (/-| |\./.test(date)) {
+            d = new Date(date)      // maybe a utc string
+        } else {
+            const tstamp = Number.parseInt(date)
+            if (tstamp) {
+                d = new Date(tstamp)
+            } else {
+                d = new Date(date)
+            }
+        }
+    }
+    return d
 }
 
 program.command('convert', {isDefault: true})
 .description('')
 .argument('[date]', 'date string or timestamp')
 .action(async (date, opts) => {
-    process.env.TZ = 'Asia/Shanghai'
-    let d
-    if (!date) {
-        d = new Date()
-    } else {
-        const tstamp = Number.parseInt(date)
-        if (tstamp) {
-            d = new Date(tstamp)
-        } else {
-            d = new Date(date)
-        }
-    }
-    print_date(d)
+    print_date(parse_date(date))
 });
 
 program.parse()
+
+module.exports = {
+    parse_date
+}
---
+++ node/test/cdate.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const cdate = require('../cdate')
+
+describe('cdate.test.js', () => {
+    it('parse timestamp', () => {
+        d = cdate.parse_date(1691648714536)
+        assert.strictEqual(d.toISOString(), '2023-08-10T06:25:14.536Z')
+    });
+
+    it('parse utc-like string', () => {
+        assert.strictEqual(cdate.parse_date('2023-02-02').toLocaleString('zh-cn'), '2023/2/2 08:00:00')
+        assert.strictEqual(cdate.parse_date('2023-02-02 12:34:56').toLocaleString('zh-cn'), '2023/2/2 12:34:56')
+        assert.strictEqual(cdate.parse_date('u2023-02-02').toLocaleString('zh-cn'), '2023/2/2 08:00:00')
+    });
+});
